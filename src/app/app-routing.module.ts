b44890bdf966ba6e593ab458757fae8e3820382a
Cameron Conway <support@cambuilt.com>
2022-07-21 16:57:26 -0400

    privacy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'boston', component: GoogleMapComponent },
   { path: 'washingtondc', component: GoogleMapComponent },
   { path: 'panorama', component: PanoramaComponent }
+  // { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
