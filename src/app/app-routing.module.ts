8e29703127911ecf4cf4fca30ccaa1675fd0c32d
Cameron Conway <support@cambuilt.com>
2022-07-21 12:08:13 -0400

    Privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CharlestonComponent } from './charleston/charleston.component';
 import { PanoramaComponent } from './panorama/panorama.component';
 import { GoogleMapComponent } from './google-map/google-map.component';
 
 const routes: Routes = [
   { path: '', component: GoogleMapComponent },
   { path: 'google', component: GoogleMapComponent },
-  { path: 'apple', component: CharlestonComponent },
   { path: 'charleston', component: GoogleMapComponent },
   { path: 'boston', component: GoogleMapComponent },
   { path: 'washingtondc', component: GoogleMapComponent },
