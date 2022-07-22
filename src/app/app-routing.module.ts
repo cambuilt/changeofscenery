9f726afc46467d124b2b1845c248672ecbf02a68
Cameron Conway <support@cambuilt.com>
2022-07-21 23:20:48 -0400

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PanoramaComponent } from './panorama/panorama.component';
 import { GoogleMapComponent } from './google-map/google-map.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
 const routes: Routes = [
   { path: '', component: GoogleMapComponent },
@@ -12,8 +10,6 @@ const routes: Routes = [
   { path: 'boston', component: GoogleMapComponent },
   { path: 'washingtondc', component: GoogleMapComponent },
   { path: 'panorama', component: PanoramaComponent },
-  { path: 'privacypolicy', component: PrivacyPolicyComponent },
-  { path: 'termsofservice', component: TermsOfServiceComponent },
   { path: '**', redirectTo: '' }
 ];
 
