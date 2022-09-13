1a171fbaf6e97ebb8e84c307f1911de79651f3e6
Cameron Conway <support@cambuilt.com>
2022-09-12 23:41:52 -0400

    align category selecteds

--- src/app/google-map/google-map.component.ts
+++ src/app/google-map/google-map.component.ts
@@ -28,7 +28,7 @@ export class GoogleMapComponent implements OnInit {
   public static places: any = [];
   public static animations: any = [];
   public static likedPlaces: any = [];
-  public static markerFilter: any = [1,2,3,4,6,7,8,9,10,11,13,14];
+  public static markerFilter: any = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
   public static placeMarkers: google.maps.Marker[] = [];
   public static placeTotal = 0;
   public static streetMarkers: google.maps.Marker[] = [];
