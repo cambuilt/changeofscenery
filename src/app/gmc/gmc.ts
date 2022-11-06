8b5a470a562d8bf7d9758bbe129fade058db89c6
Cameron Conway <support@cambuilt.com>
2022-11-06 16:48:58 -0500

    kiosk mode lat/lng

--- src/app/gmc/gmc.ts
+++ src/app/gmc/gmc.ts
@@ -342,7 +342,7 @@ export class gmc implements OnInit {
       gmc.areas.push({name:'CityCenter', lat:38.90010, lng:-77.02600, centerLat:38.90056, centerLng:-77.02513, zoom:17, heading:360, tilt:40, iconWidth:67, iconHeight:22});
       gmc.areas.push({name:'Chinatown', lat:38.90010, lng:-77.0195, centerLat:38.90056, centerLng:-77.021, zoom:17, heading:360, tilt:40, iconWidth:67, iconHeight:22});
       gmc.areas.push({name:'PennQuarter', lat:38.89731, lng:-77.02291, centerLat:38.89681, centerLng:-77.0243, zoom:17, heading:360, tilt:40, iconWidth:67, iconHeight:22});
-      gmc.areas.push({name:'FriendshipHeights', lat:38.96066, lng:-77.08571, centerLat:38.95664, centerLng:-77.08727, centerLatKiosk:38.96914, centerLngKiosk:-77.08707, zoom:15, zoomKiosk:18, heading:0, tilt:40, iconWidth:100, iconHeight:22});
+      gmc.areas.push({name:'FriendshipHeights', lat:38.96066, lng:-77.08571, centerLat:38.95664, centerLng:-77.08727, centerLatKiosk:38.96214, centerLngKiosk:-77.08707, zoom:15, zoomKiosk:18, heading:0, tilt:40, iconWidth:100, iconHeight:22});
     }
 
     gmc.areas.forEach(area => {
