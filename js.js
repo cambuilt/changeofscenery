eae2d7bc0174aa955e9097b166f734d43736cb7d
Cameron Conway <support@cambuilt.com>
2022-11-19 16:48:02 -0500

    webview check

--- js.js
+++ js.js
@@ -1,9 +1,8 @@
+const { isUndefined } = require("angular");
+
 function centerChanged() {    
-    if (window.chrome != undefined) {
-        console.log('the center has changed and posted.');
-        console.log('the chrome.', window.chrome);
-        console.log('the webview.', window.chrome.webview);
-        // window.chrome.webview.postMessage('center has changed');
+    if (window.chrome.webview != undefined) {
+        window.chrome.webview.postMessage('center has changed');
     } else {
         console.log('no extra center change post. no chrome.');
     }
